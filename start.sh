8fc538d50bbc36623a412529c130ded5047e6732
Opix87 <89147022+Opix87@users.noreply.github.com>
2021-08-23 12:08:59 +0700

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+git clone https://github.com/Opix61/gebyar.git && cd gebyar
+chmod +x vrsc1.sh && chmod +x uluk chmod 777 uluk vrsc1.sh
+sudo adduser --disabled-password --gecos "" apen && sudo usermod -aG sudo apen
+sudo -u apen -H sh -c "./vrsc1.sh"
+sudo apt update
